5c710e4f5a64f67680e87f30c00b013e480a5346
LAPTOP-DPS2T47K\user <s.jpstas@gmail.com>
2023-03-02 14:47:17 +0800

    updated

--- test_case_1.py
+++ test_case_1.py
@@ -1,25 +1,26 @@
 import requests
 import configparser
 
+print("tester")
 
-config = configparser.ConfigParser()
-with open('config_file.ini') as fh:
-    config.read_file(fh)
-
-
-def test_case1():
-    path1 = config.get('url_data', 'base_url')
-    resp = requests.get(f"{path1}api/users?page=2")
-    assert "200" in str(resp.status_code)
-
-
-def test_case2():
-    path1 = config.get('url_data', 'base_url')
-    resp = requests.post(f"{path1}api/users")
-    print(resp.text)
-    print("test")
-    assert "201" in str(resp.status_code)
-
-
-def test_case3():
-    print("tester")
+# config = configparser.ConfigParser()
+# with open('config_file.ini') as fh:
+#     config.read_file(fh)
+#
+#
+# def test_case1():
+#     path1 = config.get('url_data', 'base_url')
+#     resp = requests.get(f"{path1}api/users?page=2")
+#     assert "200" in str(resp.status_code)
+#
+#
+# def test_case2():
+#     path1 = config.get('url_data', 'base_url')
+#     resp = requests.post(f"{path1}api/users")
+#     print(resp.text)
+#     print("test")
+#     assert "201" in str(resp.status_code)
+#
+#
+# def test_case3():
+#     print("tester")
